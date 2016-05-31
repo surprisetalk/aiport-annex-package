6a6dff2aa37336ad51afb8356a16534599ca014c
Taylor Sarrafian <surprisetalk@gmail.com>
2016-05-31 02:01:29 +0000

    😬

--- package.js
+++ package.js
@@ -1,6 +1,18 @@
 
-// TODO: require aiport-dev
+var annex = require('../aiport-dev/dev.js').annex;
+var aiport_package = require('../aiport-package/package.js');
 
-module.exports = {
-    index: "node_modules/aiport-annex-package/package.jade"
-};
+var package = annex( 
+    __dirname + "/package.jade",
+    query => query.type in aiport_package.installed() && ~aiport_package.installed()[ query.type ].indexOf( query.name )
+	? Promise.resolve( { type: query.type, name: query.name, installed: true } )
+	: Promise.reject( { code: 400, msg: "could not find package: " + query.name } ) );
+
+var packages = annex( 
+    __dirname + "/packages.jade",
+    query => aiport_package.available().then( available => ({ installed: aiport_package.installed(), available: available }) ),
+    { package: package } );
+
+module.exports = packages;
+
+
