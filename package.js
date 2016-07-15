b00e25ed1c1d6a3eaf878ffaa85bac25f5e8b9e8
Taylor Sarrafian <surprisetalk@gmail.com>
2016-07-15 06:29:03 +0000

    bulk commit

--- package.js
+++ package.js
@@ -2,11 +2,16 @@
 var annex = require('../aiport-dev/dev.js').annex;
 var aiport_package = require('../aiport-package/package.js');
 
+var package_exists = 
+    ( source, package_type, package_name ) =>
+	package_type in source && ~source[ package_type ].indexOf( package_name );
+
 var package = annex( 
     __dirname + "/package.jade",
-    query => query.type in aiport_package.installed() && ~aiport_package.installed()[ query.type ].indexOf( query.name )
-	? Promise.resolve( { type: query.type, name: query.name, installed: true } )
-	: Promise.reject( { code: 400, msg: "could not find package: " + query.name } ) );
+    query => aiport_package.available().then( 
+	available => package_exists( available, query.type, query.name )
+	    ? Promise.resolve( { type: query.type, name: query.name, installed: package_exists( aiport_package.installed(), query.type, query.name ) } ) 
+	    : Promise.reject( { code: 400, msg: "could not find package in npm: " + query.type + "-" + query.name } ) ) );
 
 var packages = annex( 
     __dirname + "/packages.jade",
